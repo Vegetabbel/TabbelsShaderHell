486174647935bb346e8db8ede2576e14bbe6f5f9
Veikka Huttunen <veikka.huttunen@hotmail.fi>
2020-12-24 13:58:59 +0200

    Initial commit

---
+++ Assets/Scripts/FloatAndRotate.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class FloatAndRotate : MonoBehaviour
+{
+    public float floatAmplitude = 0.5f;
+    public float floatSpeed = 1f;
+    public bool randomizeStartFloatProgression = true;
+    private float startFloatProgression = 0f;
+    private float startY;
+
+    public float maxRotationSpeed = 1f;
+    private float rotationSpeed;
+    public Space rotateWorldOrLocalSpace = Space.World;
+    public bool randomizeInitialRotationProgression = true;
+    public bool rotateX = false;
+    public bool rotateY = true;
+    public bool rotateZ = false;
+
+
+    void Start()
+    {
+        startY = transform.localPosition.y;
+        RandomizeInitialFloatProgression();
+        RandomizeRotationSpeed();
+        RandomizeInitialRotationProgession();
+    }
+
+    void Update()
+    {
+        Float();
+        Rotate();
+    }
+    void RandomizeInitialFloatProgression()
+    {
+        if (randomizeStartFloatProgression)
+        {
+            startFloatProgression = Random.Range(-1f, 1f);
+
+            transform.localPosition =
+                new Vector3(transform.localPosition.x,
+                            startY + floatAmplitude * Mathf.Sin((Time.time + startFloatProgression) * floatSpeed),
+                            transform.localPosition.z);
+        }
+    }
+
+    void Float()
+    {
+        transform.localPosition =
+        new Vector3(transform.localPosition.x,
+                    startY + floatAmplitude * Mathf.Sin((Time.time + startFloatProgression) * floatSpeed),
+                    transform.localPosition.z);
+    }
+    void RandomizeRotationSpeed()
+    {
+        rotationSpeed = Random.Range(-maxRotationSpeed, maxRotationSpeed);
+    }
+    void RandomizeInitialRotationProgession()
+    {
+        Vector3 rotateAmount = new Vector3(0f, 0f, 0f);
+
+        if (rotateX)
+        {
+            rotateAmount.x = Random.Range(0f, 360f);
+        }
+        if (rotateY)
+        {
+            rotateAmount.y = Random.Range(0f, 360f);
+        }
+        if (rotateZ)
+        {
+            rotateAmount.z = Random.Range(0f, 360f);
+        }
+
+        transform.Rotate(rotateAmount);
+    }
+    void Rotate()
+    {
+        float rotateAmount = rotationSpeed * Time.deltaTime;
+
+        if (rotateX)
+        {
+            transform.Rotate(new Vector3(rotateAmount, 0f, 0f), rotateWorldOrLocalSpace);
+        }
+        if (rotateY)
+        {
+            transform.Rotate(new Vector3(0f, rotateAmount, 0f), rotateWorldOrLocalSpace);
+        }
+        if (rotateZ)
+        {
+            transform.Rotate(new Vector3(0f, 0f, rotateAmount), rotateWorldOrLocalSpace);
+        }
+    }
+}
---
+++ Assets/Shaders/Unlit3D1.shader
@@ -0,0 +1,77 @@
+﻿Shader "Unlit/Unlit3D1"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Angle ("Angle", Float) = 10
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            float _Angle;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+
+                int directionMultiplier = 1;
+                float timeSin = sin(_Time[1]);
+
+                //_Angle = abs(timeSin) * _Angle;
+
+                //if (timeSin < 0) directionMultiplier = -1;
+                //else directionMultiplier = 1;
+                
+                //float dist = distance(float3(0,0,0), v.vertex.y);
+
+                //v.vertex.y += dist;
+
+                _Angle = _Angle * v.vertex.y;
+
+                v.vertex.xz = float2(   v.vertex.x * cos(_Angle ) - v.vertex.z * sin(_Angle) * directionMultiplier,
+                                        v.vertex.z * cos(_Angle) + v.vertex.x * sin(_Angle) * directionMultiplier
+                                    );
+
+                //v.vertex.y -= dist;
+
+
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
